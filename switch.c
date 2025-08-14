3c1fbca9b1f17df4f516e62136d09ed6fd687090
TheAsiyaS <theasiya.s@gmail.com>
2025-08-15 05:04:48 +0530

    if else , nested if - solved

---
+++ switch.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+// average calculate
+int main()
+{
+    int option;
+
+    printf("Select your option.\n1.Protta\n2.Mandhi\n3.Fried rice\n4.Alfarm");
+    scanf("%d", &option);
+  switch (option)
+  {
+  case 1:
+    /* code */
+    break;
+    case 2:
+    /* code */
+    break;
+      case 3:
+    /* code */
+    break;
+      case 4:
+    /* code */
+    break;
+
+  default:
+  //error mesage
+    break;
+  }
+    return 0;
+}
