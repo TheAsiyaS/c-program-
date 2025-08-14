6bdbc09bb872af92356998665abf10263ed75db2
TheAsiyaS <theasiya.s@gmail.com>
2025-08-15 01:01:23 +0530

    swapping - solved

---
+++ swapping.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1=15, num2=49,temp;
+temp=num1;
+num1=num2;
+num2=temp;
+printf("number 1 : %d -- number 2 : %d",num1,num2);
+    
+
+    return 0;
+}
