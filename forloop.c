80a0b06b5ef81a945e6601d702a6ccf520a80996
TheAsiyaS <theasiya.s@gmail.com>
2025-08-15 06:00:01 +0530

    loop- sum - solved

---
+++ forloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// average calculate
+int main()
+{
+int i,sum ;
+
+for (i=0;i<10;i++){
+sum=sum+i;//here previous sum is stored |logic can use in hacker rank , series problem|
+printf("sum is : %d",&sum);
+}
+printf("final sum is : %d",&sum);
+
+
+    return 0;
+}  
